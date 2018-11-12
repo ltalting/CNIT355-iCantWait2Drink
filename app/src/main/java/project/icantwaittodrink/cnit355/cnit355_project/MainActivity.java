fd8f2b5bbd51a8cc0e8d25ddb80b68acdd6553f4
molson82 <will@pal-nat186-156-138.itap.purdue.edu>
2018-11-12 16:36:23 -0500

    blank project

---
+++ app/src/main/java/project/icantwaittodrink/cnit355/cnit355_project/MainActivity.java
@@ -0,0 +1,13 @@
+package project.icantwaittodrink.cnit355.cnit355_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
