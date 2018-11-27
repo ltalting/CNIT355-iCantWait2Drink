592fa96e1c4be02b9546c726f4fb9fdbf4c21263
molson82 <will@Williams-MBP.lfytina2.metronetinc.net>
2018-11-26 21:03:16 -0500

    starting the ui with fragments

---
+++ app/src/main/java/project/icantwaittodrink/cnit355/cnit355_project/MainMenu.java
@@ -0,0 +1,17 @@
+package project.icantwaittodrink.cnit355.cnit355_project;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainMenu extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_menu, container, false);
+    }
+}
