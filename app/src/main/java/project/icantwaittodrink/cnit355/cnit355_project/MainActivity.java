85585d4597365b3a8d0804886cde986c3696bd22
molson82 <will@Williams-MBP.lfytina2.metronetinc.net>
2018-11-27 00:05:28 -0500

    using intents instead of fragments. This is starting to work

--- app/src/main/java/project/icantwaittodrink/cnit355/cnit355_project/MainActivity.java
+++ app/src/main/java/project/icantwaittodrink/cnit355/cnit355_project/MainActivity.java
@@ -1,13 +1,27 @@
 package project.icantwaittodrink.cnit355.cnit355_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView bobbyImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bobbyImg = findViewById(R.id.bobby);
+        bobbyImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), BobbT.class);
+                startActivity(intent);
+            }
+        });
     }
 }
