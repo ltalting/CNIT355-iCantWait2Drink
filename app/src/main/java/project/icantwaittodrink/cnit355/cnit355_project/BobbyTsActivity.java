3578fe87d561c62870888bfb52bb9b6daadbc443
stucker26 <tucker58@purdue.edu>
2018-11-30 23:41:02 -0500

    Timer In Progress

--- app/src/main/java/project/icantwaittodrink/cnit355/cnit355_project/BobbyTsActivity.java
+++ app/src/main/java/project/icantwaittodrink/cnit355/cnit355_project/BobbyTsActivity.java
@@ -1,13 +1,29 @@
 package project.icantwaittodrink.cnit355.cnit355_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class BobbyTsActivity extends AppCompatActivity {
 
+    Button bobbyTimer;
+    ImageView bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bobby_ts);
+
+        bt = findViewById(R.id.btnBobbyTimer);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), TimerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/project/icantwaittodrink/cnit355/cnit355_project/TimerActivity.java
+++ app/src/main/java/project/icantwaittodrink/cnit355/cnit355_project/TimerActivity.java
@@ -51,8 +51,5 @@ public class TimerActivity extends AppCompatActivity {
             }
         });
 
-
     }
-
-
 }
